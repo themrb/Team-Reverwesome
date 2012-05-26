32f0165933c718c9e3a70b1d0470567eb56d8df4
Jarrah <themrb4@gmail.com>
2012-05-26 16:57:56 +1000

    storeweights implemented

--- src/agent.ads
+++ src/agent.ads
@@ -1,6 +1,8 @@
 package Agent is
 
-	procedure Ada_Subroutine;
-	pragma export(CPP, Ada_Subroutine );
+   procedure Ada_Subroutine;
+   pragma export(CPP, Ada_Subroutine );
+
+   testint : Integer := 0;
 
 end Agent;
